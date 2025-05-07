58e55a99ed857bb74afb8014d54a1fdbac2fa56b
Kiran Roge <kiranroge19@gmail.com>
2025-05-07 02:18:35 -0500

    changed pictures on the landing page, changed styles

--- ecosnap/settings.py
+++ ecosnap/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # You should generate a new secret key for your project.
 # For development, this is a placeholder.
-SECRET_KEY = 'django-insecure-temporary-secret-key-for-development'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
